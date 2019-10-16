625e1b0b6ae5eca174304debb2ca0ce0bc26179b
Felix-Suen <felix.suen2000@gmail.com>
2019-10-15 22:40:28 -0400

    Broken Arduino

--- Clock/Clock.ino
+++ Clock/Clock.ino
@@ -1,21 +1,20 @@
-// include the library code:
 #include <LiquidCrystal.h>
 #include <Wire.h>
 #include "RTClib.h"
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+//LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
 RTC_DS1307 rtc;
-char daysOfTheWeek[7][12] = {"Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 void setup() {
   // set up the LCD's number of columns and rows:
   Wire.begin();
   rtc.begin();
   lcd.begin(16, 2);
-  //if (!rtc.isrunning()) {
-  rtc.adjust(DateTime(2019, 10, 14, 23, 43, 0));
-  //}
+  if (!rtc.isrunning()) {
+    rtc.adjust(DateTime(2019, 10, 15, 23, 59, 30));
+  }
 }
 
 void loop() {
@@ -27,10 +26,9 @@ void loop() {
   lcd.print(now.month(), DEC);
   lcd.print('/');
   lcd.print(now.day(), DEC);
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
+
+  //second line
   lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
   lcd.print("Time: ");
   int hour = now.hour();
   String ampm = "";
@@ -49,4 +47,5 @@ void loop() {
   lcd.setCursor(14, 1);
   lcd.print(ampm);
   delay(1000);
+  lcd.clear();
 }
